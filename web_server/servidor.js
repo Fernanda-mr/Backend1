078a08ddbd110737c278f202427f5f41e4b1f43f
Fernanda-mr <66288449+Fernanda-mr@users.noreply.github.com>
2022-04-28 11:34:00 -0600

    commit cuatro: agregamos mini tipos a usar

--- web_server/servidor.js
+++ web_server/servidor.js
@@ -9,4 +9,15 @@ http.createServer(function (request, response){
     if (filePath == './'){
         filePath = './index.html'; //le indicamos al servidore que busque dicho archivo
     }
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var ContentType = 'text/html';
+    var mimeTypes = {
+        //son los mini tipos, porque es importante que se tomen en cuenta en un servidor
+        '.html':'text/html',
+        '.js':'text/javascript',
+        '.css':'text/css',
+        '.png':'image/png',
+        '.jpg':'image/jpg',
+        '.gif':'image/gif',
+    }
 })
