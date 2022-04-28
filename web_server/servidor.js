790aad581bca29f7bbb884ac89cd477f7294e039
Fernanda-mr <66288449+Fernanda-mr@users.noreply.github.com>
2022-04-28 11:52:31 -0600

    commit cinco: envios de error 404, 200 y 500

--- web_server/servidor.js
+++ web_server/servidor.js
@@ -20,4 +20,28 @@ http.createServer(function (request, response){
         '.jpg':'image/jpg',
         '.gif':'image/gif',
     }
-})
+    //template de error 404
+    //si existe un error manda el 404, si todo esta bien manda el 200 y un error 500 si es error del servidor 
+    
+    fs.readFileSync(filePath, function(error, content){
+        if(error){
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content){
+                    response.writeHead(200, {'Content-Type': ContentType});
+                    response.end(content, 'utf-8');
+                });
+            }
+            else{
+                response.writeHead(500);
+                response.end('Sorry, check with the site admi for error: '+ error.code + '..\n');
+                response.end();
+            }
+        }
+        else{
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        }
+    });
+
+}) .listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
