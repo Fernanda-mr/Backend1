55aea518606fadcd590e8d8cb2cb971f0f4ee766
Fernanda-mr <66288449+Fernanda-mr@users.noreply.github.com>
2022-05-02 18:21:13 -0600

    commit ocho: cambios de corrección en mimetypes

--- web_server/servidor.js
+++ web_server/servidor.js
@@ -1,49 +1,56 @@
-var http=require('http');
+var http = require('http');
 var fs = require('fs');
 var path = require('path');
-
-http.createServer(function (request, response){
-    console.log('request', request.url);
-
+ 
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+ 
     var filePath = '.' + request.url;
-    if (filePath == './'){
-        filePath = './index.html'; //le indicamos al servidore que busque dicho archivo
+    if (filePath == './') {
+        filePath = './index.html';
     }
+ 
     var extname = String(path.extname(filePath)).toLowerCase();
-    var ContentType = 'text/html';
+    var contentType = 'text/html';
     var mimeTypes = {
-        //son los mini tipos, porque es importante que se tomen en cuenta en un servidor
-        '.html':'text/html',
-        '.js':'text/javascript',
-        '.css':'text/css',
-        '.png':'image/png',
-        '.jpg':'image/jpg',
-        '.gif':'image/gif'
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
     };
-    //template de error 404
-    //si existe un error manda el 404, si todo esta bien manda el 200 y un error 500 si es error del servidor 
-    
+ 
     contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFileSync(filePath, function(error, content){
-        if(error){
+ 
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
             if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content){
-                    response.writeHead(200, {'Content-Type': ContentType});
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
                     response.end(content, 'utf-8');
                 });
             }
-            else{
+            else {
                 response.writeHead(500);
-                response.end('Sorry, check with the site admi for error: '+ error.code + '..\n');
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
                 response.end();
             }
         }
-        else{
-            response.writeHead(200, {'Content-Type': contentType});
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
             response.end(content, 'utf-8');
         }
     });
+ 
+}).listen(3000);
 
-}) .listen(3000);
 console.log('Server running at http://127.0.0.1:3000/');
